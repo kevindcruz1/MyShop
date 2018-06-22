1aa38801d74f29a8e027b3cf3fbcdab765864a14
zopnow <y.kevindcruz202@gmail.com>
2018-06-22 19:07:19 +0530

    Made changes in admin

---
+++ admin_area/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <title>Welcome to admin area</title>
+        
+        <link rel="stylesheet" href="styles/style.css" media="all"/>
+        
+    </head>
+    
+    <body>
+        
+        
+        <div class="wrapper">
+            
+            <div class="header"></div>
+            <div class="left_sidebar"></div>
+            
+            
+            
+            
+            
+            
+            
+            
+        </div>
+        
+        
+        
+    </body>
+    
+    
+</html>
